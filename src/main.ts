387b781f54c9129e3b8669c8284787130c833372
max <lettnermaximilian2008@gmail.com>
2025-11-10 07:02:59 +0100

    changed the genaral functions of the code to be an orthographic viewpoint

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,30 @@
 import * as THREE from 'three';
 
 const scene : THREE.Scene = new THREE.Scene();
-const camera : THREE.PerspectiveCamera = new THREE.PerspectiveCamera(
-  75, window.innerWidth / window.innerHeight, 0.1, 1000
-);
 const renderer :THREE.WebGLRenderer = new THREE.WebGLRenderer();
-
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-const geometry : THREE.BoxGeometry = new THREE.BoxGeometry();
-const edge : THREE.EdgesGeometry = new THREE.EdgesGeometry(geometry);
-const line : THREE.LineSegments = new THREE.LineSegments(edge);
+const aspect : number = window.innerWidth / window.innerHeight;
+const size :number = 1;
 
-
-const cube : THREE.Mesh = new THREE.Mesh(
-  geometry,
-
-  new THREE.MeshBasicMaterial({ color: 0x0000FF })
+const camera : THREE.OrthographicCamera = new THREE.OrthographicCamera(
+  -size * aspect,  //l
+  size * aspect,   //r
+  size,            //o
+  -size,           //u
+  0.1,             
+  1000             
 );
-//scene.add(cube);
-
-camera.position.z = 2;
 
 
-//cube.rotation.x = 120;
-cube.rotation.y = 120;
-cube.rotation.z = 120;
+const geometry : THREE.BoxGeometry = new THREE.BoxGeometry(); //creates a cube-like structure
+const edge : THREE.EdgesGeometry = new THREE.EdgesGeometry(geometry); //gets the edges of this cube
+const line : THREE.LineSegments = new THREE.LineSegments(edge); // draws lines on that edges
+
 scene.add(line);
+
+camera.position.set(2, 1, 2);
+camera.lookAt(0, 0, 0);
+
 renderer.render(scene,camera);
-
-
-//animate();
